7c6b7b9ba7bebd2e385649e121d01d0c044c0631
Shivangi-UG <shivangi.unicoglobal@gmail.com>
2022-07-30 13:40:47 +0530

    basic functionalities done

--- flow-chart/src/App.js
+++ flow-chart/src/App.js
@@ -1,6 +1,11 @@
-
-import { useState } from "react";
-import ReactFlow, {Background} from "react-flow-renderer";
+import { useState, useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  Background,
+  Controls,
+} from "react-flow-renderer";
 
 const initialNodes = [
   {
@@ -18,37 +23,79 @@ const initialNodes = [
   },
   {
     id: "3",
-    type: "output",
     data: { label: "Output Node" },
     position: { x: 250, y: 250 },
   },
   {
     id: "4",
-    data: { label: ""},
-    position: {x: 600, y: 100},
+    data: { label: <div>Default Node4</div> },
+    position: { x: 50, y: 700 },
   },
+  {
+    id: "5",
+    data: { label: <div>Default Node5</div> },
+    position: { x: 300, y: 200 },
+  },
+  {
+    id: "6",
+    data: { label: <div>Default Node6</div> },
+    position: { x: 450, y: 500 },
+  },
+  {
+    id: "7",
+    data: { label: <div>Default Node7</div> },
+    position: { x: 500, y: 600 },
+  }
 ];
 
 const initialEdges = [
   { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3", animated: true },
-  {id: "e1-4", source: "3", target: "4"}
+  { id: "e2-3", source: "2", target: "3" },
+  { id: "e3-4", source: "3", target: "4" },
+  { id: "e4-5", source: "4", target: "5" },
+  { id: "e5-6", source: "5", target: "6" },
+  
 ];
 
 function Flow() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
 
-  return <ReactFlow style = {{width: "100vw", height: "80vh"}} nodes={nodes} edges={edges} fitView>
-    <Background/>
-  </ReactFlow>;
-}
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
 
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((edg) => applyEdgeChanges(changes, edg)),
+    [setEdges]
+  );
+
+  const onConnect = useCallback(
+    (connection) => setEdges((edgs) => addEdge(connection,edges)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      style={{ width: "100vw", height: "90vh" }}
+      nodes={nodes}
+      edges={edges}
+      onNodesChange = {onNodesChange}
+      onEdgesChange = {onEdgesChange}
+      onConnect = {onConnect}
+      fitView
+    >
+      <Background />
+      <Controls />
+    </ReactFlow>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <Flow/>
+      <Flow />
     </div>
   );
 }
